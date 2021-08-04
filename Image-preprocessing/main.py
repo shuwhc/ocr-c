1246fd19d5900b3ac0360d2ce7608b45ac509464
shuwhc <bstage001@gmail.com>
2021-08-04 16:53:00 +0800

    commit-venv

---
+++ Image-preprocessing/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import skimage.measure
+
+a = np.array([
+      [  20,  200,   -5,   23],
+      [ -13,  134,  119,  100],
+      [ 120,   32,   49,   25],
+      [-120,   12,    9,   23]
+])
+
+
+print(a)
