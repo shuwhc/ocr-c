184abed3d778fd4a643659967abce70a926f33fa
Shu <bstage001@gmail.com>
2021-08-06 14:18:56 +0800

    com

--- Prepro/cv2open_test.py
+++ Prepro/cv2open_test.py
@@ -1,3 +1,11 @@
+# pip install --upgrade pip
+# pip install matplotlib
+# pip install numpy
+#pip install pillow
+# pip install opencv-python
+
+#3.8.5 
+
 import cv2
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -12,7 +20,7 @@ img = cv2.imread(img_file)
 #function - turn img to greyscale
 #def grayscale(image): 
     #return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#gray_img = grayscale(img)
+#gray_img = grayscale(img)``
 #cv2.imwrite("Prepro\SemiData\gray.jpg", gray_img)
 
 ##
@@ -23,9 +31,49 @@ grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
   
 (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
  
-cv2.imshow('Black white image', blackAndWhiteImage)
+#cv2.imshow('Black white image', blackAndWhiteImage)
 #cv2.imshow('Original image',originalImage)
 cv2.imshow('Gray image', grayImage)
   
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+    newImage = cvImage.copy()
+    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Apply dilate to merge text into meaningful lines/paragraphs.
+    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+    # But use smaller kernel on Y axis to separate between different blocks of text
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+    # Find all contours
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    for c in contours:
+        rect = cv2.boundingRect(c)
+        x,y,w,h = rect
+        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # Find largest contour and surround in min area box
+    largestContour = contours[0]
+    print (len(contours))
+    minAreaRect = cv2.minAreaRect(largestContour)
+    cv2.imwrite("temp/boxes.jpg", newImage)
+    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+    angle = minAreaRect[-1]
+    if angle < -45:
+        angle = 90 + angle
+    return -1.0 * angle
+# Rotate the image around its center
+def rotateImage(cvImage, angle: float):
+    newImage = cvImage.copy()
+    (h, w) = newImage.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return newImage
