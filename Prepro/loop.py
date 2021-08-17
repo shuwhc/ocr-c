2c5e1c51e09067f85cd991b7f54fd5cfc7bb2d53
Shu <bstage001@gmail.com>
2021-08-17 11:48:29 +0800

    commit-shu-17aug

---
+++ Prepro/loop.py
@@ -0,0 +1,13 @@
+import os
+directory = 'C:\IA\ocr-c\Prepro\Data1'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg"):
+        
+        
+        
+     
+      continue
+    else:
+        
+        continue
